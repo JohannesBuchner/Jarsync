81acea4275bc09b00690c32fa05cee3508a974e8
rsdio <rsdio@388fd5d1-5327-0410-80e4-b6d47a689578>
2002-08-18 17:37:49 +0000

    JavaDocs, added fileChecksum, moved toString(byte[]) to this class from MD4.
    git-svn-id: https://jarsync.svn.sourceforge.net/svnroot/jarsync@37 388fd5d1-5327-0410-80e4-b6d47a689578

--- trunk/jarsync/source/org/metastatic/rsync/MessageDigest.java
+++ trunk/jarsync/source/org/metastatic/rsync/MessageDigest.java
@@ -1,4 +1,4 @@
-// :vim:set tw=78 expandtab tabstop=3:
+// :vim:set tw=72 expandtab tabstop=3:
 // $Id$
 //
 // Derived from gnu.crypto.hash.BaseHash from the classpathx-crypto
@@ -30,6 +30,10 @@
 
 package org.metastatic.rsync;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * <p>A base abstract class to facilitate hash implementations.</p>
  *
@@ -39,6 +43,8 @@ public abstract class MessageDigest {
 
    // Constants and variables
    // -------------------------------------------------------------------------
+
+   public static final String RCS_ID = "$Id$";
 
    /** The name of this digest. */
    protected String name;
@@ -64,18 +70,38 @@ public abstract class MessageDigest {
    // Instance methods
    // -------------------------------------------------------------------------
 
+   /**
+    * Return the cononical name of this message digest.
+    *
+    * @return This digest's name.
+    */
    public String name() {
       return name;
    }
 
+   /**
+    * Return the size of the final hash, in bytes.
+    *
+    * @return The hash size.
+    */
    public int hashSize() {
       return hashSize;
    }
 
+   /**
+    * Return the internal block size of this message digest.
+    *
+    * @return The block size.
+    */
    public int blockSize() {
       return blockSize;
    }
 
+   /**
+    * Update the digest with a single byte.
+    *
+    * @param b The byte to add.
+    */
    public void update(byte b) {
       // compute number of bytes still unhashed; ie. present in buffer
       int i = (int)(count % blockSize);
@@ -86,6 +112,13 @@ public abstract class MessageDigest {
       }
    }
 
+   /**
+    * Update the digest with a portion of a byte array.
+    *
+    * @param b The bytes to add.
+    * @param offset From whence in <code>b</code> to start.
+    * @param len The number of bytes to read from <code>b</code>.
+    */
    public void update(byte[] b, int offset, int len) {
       int n = (int)(count % blockSize);
       count += len;
@@ -106,6 +139,12 @@ public abstract class MessageDigest {
       }
    }
 
+   /**
+    * Finish processing the input and return the computed digest. After
+    * calling this method this instance is {@link #reset()}.
+    *
+    * @return A byte array containing the computed digest.
+    */
    public byte[] digest() {
       byte[] tail = padBuffer(); // pad remaining bytes in buffer
       update(tail, 0, tail.length); // last transform of a message
@@ -116,7 +155,10 @@ public abstract class MessageDigest {
       return result;
    }
 
-   public void reset() { // reset this instance for future re-use
+   /**
+    * Reset this instance for future re-use.
+    */
+   public void reset() {
       count = 0L;
       for (int i = 0; i < blockSize; ) {
          buffer[i++] = 0;
@@ -125,18 +167,47 @@ public abstract class MessageDigest {
       resetContext();
    }
 
+   /**
+    * Compute the digest for an entire file.
+    *
+    * @param f The file to digest.
+    * @return The digest computed from <code>f</code>.
+    * @throws IOException If reading from <code>f</code> fails.
+    */
+   public byte[] fileChecksum(File f) throws IOException {
+      reset();
+      FileInputStream in = new FileInputStream(f);
+      byte[] buf = new byte[blockSize];
+      int len;
+      while ((len = in.read(buf)) != -1) {
+         update(buf, 0, len);
+      }
+      return digest();
+   }
+
    // methods to be implemented by concrete subclasses ------------------------
 
+   /**
+    * Clone this instance. The internal state (the number of bytes read
+    * and the digest thusfar computed) are copied into the new instance.
+    *
+    * @return A clone of this instance.
+    */
    public abstract Object clone();
 
+   /**
+    * Perform a simple conformance test of this message digest.
+    *
+    * @return true If the internal self-test succeeds.
+    */
    public abstract boolean selfTest();
 
    /**
     * <p>Returns the byte array to use as padding before completing a hash
     * operation.</p>
     *
-    * @return the bytes to pad the remaining bytes in the buffer before
-    * completing a hash operation.
+    * @return The bytes to pad the remaining bytes in the buffer before
+    *    completing a hash operation.
     */
    protected abstract byte[] padBuffer();
 
@@ -153,9 +224,25 @@ public abstract class MessageDigest {
    /**
     * <p>The block digest transformation per se.</p>
     *
-    * @param in the <i>blockSize</i> long block, as an array of bytes to digest.
-    * @param offset the index where the data to digest is located within the
+    * @param in The <i>blockSize</i> long block, as an array of bytes to digest.
+    * @param offset The index where the data to digest is located within the
     * input buffer.
     */
    protected abstract void transform(byte[] in, int offset);
+
+   // From gnu.crypto.util.Util
+
+   private static final char[] HEX_DIGITS = {
+      '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'
+   };
+
+   public static String toString(byte[] b) {
+      char[] buf = new char[b.length * 2];
+      for (int i = 0, j = 0, k; i < b.length; ) {
+         k = b[i++];
+         buf[j++] = HEX_DIGITS[(k >>> 4) & 0x0F];
+         buf[j++] = HEX_DIGITS[ k        & 0x0F];
+      }
+      return new String(buf);
+   }
 }
