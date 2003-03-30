8483fd806929423c31e154e379dcc6aa1b763ed4
rsdio <rsdio@388fd5d1-5327-0410-80e4-b6d47a689578>
2003-03-30 15:31:33 +0000

    + Formatting change
    git-svn-id: https://jarsync.svn.sourceforge.net/svnroot/jarsync@111 388fd5d1-5327-0410-80e4-b6d47a689578

--- trunk/jarsync/source/org/metastatic/rsync/Rebuilder.java
+++ trunk/jarsync/source/org/metastatic/rsync/Rebuilder.java
@@ -1,47 +1,45 @@
-// vim:set tw=72 expandtab softtabstop=3 shiftwidth=3 tabstop=3:
-// $Id$
-//
-// Rebuilder -- File reconstruction from deltas.
-// Copyright (C) 2001,2002,2003  Casey Marshall <rsdio@metastatic.org>
-//
-// This file is a part of Jarsync.
-//
-// Jarsync is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2, or (at your option) any
-// later version.
-//
-// Jarsync is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Jarsync; see the file COPYING.  If not, write to the
-//
-//    Free Software Foundation Inc.,
-//    59 Temple Place - Suite 330,
-//    Boston, MA 02111-1307
-//    USA
-//
-// Linking this library statically or dynamically with other modules is
-// making a combined work based on this library.  Thus, the terms and
-// conditions of the GNU General Public License cover the whole
-// combination.
-//
-// As a special exception, the copyright holders of this library give
-// you permission to link this library with independent modules to
-// produce an executable, regardless of the license terms of these
-// independent modules, and to copy and distribute the resulting
-// executable under terms of your choice, provided that you also meet,
-// for each linked independent module, the terms and conditions of the
-// license of that module.  An independent module is a module which is
-// not derived from or based on this library.  If you modify this
-// library, you may extend this exception to your version of the
-// library, but you are not obligated to do so.  If you do not wish to
-// do so, delete this exception statement from your version.
-//
-// --------------------------------------------------------------------------
+/* vim:set softtabstop=3 shiftwidth=3 tabstop=3 expandtab tw=72:
+   $Id$
+
+   Rebuilder: File reconstruction from deltas.
+   Copyright (C) 2003  Casey Marshall <rsdio@metastatic.org>
+  
+   This file is a part of Jarsync.
+  
+   Jarsync is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2 of the License, or (at
+   your option) any later version.
+  
+   Jarsync is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with Jarsync; if not, write to the
+  
+      Free Software Foundation, Inc.,
+      59 Temple Place, Suite 330,
+      Boston, MA  02111-1307
+      USA
+  
+   Linking Jarsync statically or dynamically with other modules is
+   making a combined work based on Jarsync.  Thus, the terms and
+   conditions of the GNU General Public License cover the whole
+   combination.
+  
+   As a special exception, the copyright holders of Jarsync give you
+   permission to link Jarsync with independent modules to produce an
+   executable, regardless of the license terms of these independent
+   modules, and to copy and distribute the resulting executable under
+   terms of your choice, provided that you also meet, for each linked
+   independent module, the terms and conditions of the license of that
+   module.  An independent module is a module which is not derived from
+   or based on Jarsync.  If you modify Jarsync, you may extend this
+   exception to your version of it, but you are not obligated to do so.
+   If you do not wish to do so, delete this exception statement from
+   your version.  */
 
 package org.metastatic.rsync;
 
@@ -70,17 +68,6 @@ public class Rebuilder
    /** The suffix for temporary files. */
    private static final String TMP_SUFFIX = ".temp";
 
-   // Inner class.
-   // -----------------------------------------------------------------------
-
-   public static interface OutputCallback {
-
-      /**
-       * Write <i>len</i> bytes from <i>buf</i> at offset <i>offset</i>.
-       */
-      void write(long offset, byte[] buf, int len);
-   }
-
    // Class methods.
    // -----------------------------------------------------------------------
 
@@ -91,7 +78,7 @@ public class Rebuilder
     * @param deltas The deltas to apply.
     * @return The reconstructed data.
     */
-   public static byte[] rebuild(byte[] buf, Collection deltas) {
+   public static byte[] rebuild(byte[] buf, List deltas) {
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
       try {
          rebuild(baos, buf, deltas);
@@ -107,7 +94,7 @@ public class Rebuilder
     * @param buf The original data.
     * @param deltas The deltas to apply.
     */
-   public static void rebuild(OutputStream out, byte[] buf, Collection deltas)
+   public static void rebuild(OutputStream out, byte[] buf, List deltas)
    throws IOException
    {
       Delta[] darray = (Delta[]) deltas.toArray(new Delta[0]);
@@ -131,7 +118,7 @@ public class Rebuilder
     * @param deltas  The deltas to apply.
     */
    public static void
-   rebuild(OutputStream out, File oldFile, Collection deltas)
+   rebuild(OutputStream out, File oldFile, List deltas)
    throws IOException
    {
       RandomAccessFile f = new RandomAccessFile(oldFile, "r");
@@ -164,7 +151,7 @@ public class Rebuilder
     *    file.
     * @return A unique {@link java.io.File} containing the reconstruction.
     */
-   public static File rebuildFile(File oldFile, Collection deltas)
+   public static File rebuildFile(File oldFile, List deltas)
    throws IOException
    {
       File newFile = File.createTempFile(TMP_PREFIX, TMP_SUFFIX,
@@ -181,7 +168,7 @@ public class Rebuilder
     *    different file than <code>oldFile</code>
     * @param deltas The {@link Delta}s to apply.
     */
-   public static void rebuildFile(File oldFile, File newFile, Collection deltas)
+   public static void rebuildFile(File oldFile, File newFile, List deltas)
    throws IOException
    {
       if (oldFile.equals(newFile)) {
@@ -226,7 +213,7 @@ public class Rebuilder
     * @param file The file to reconstruct.
     * @param deltas The {@link Delta}s to apply.
     */
-   public static void rebuildFileInPlace(File file, Collection deltas)
+   public static void rebuildFileInPlace(File file, List deltas)
    throws IOException
    {
       final boolean copyOnly = !file.exists();
