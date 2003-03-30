39c3187bceb54c20b3e47629274f0570180cde20
rsdio <rsdio@388fd5d1-5327-0410-80e4-b6d47a689578>
2003-03-30 15:38:17 +0000

    + Changed to test streaming matcher
    git-svn-id: https://jarsync.svn.sourceforge.net/svnroot/jarsync@113 388fd5d1-5327-0410-80e4-b6d47a689578

--- trunk/jarsync/test/gnu/testlet/org/metastatic/rsync/SimpleTest.java
+++ trunk/jarsync/test/gnu/testlet/org/metastatic/rsync/SimpleTest.java
@@ -1,47 +1,45 @@
-// vim:set softtabstop=3 shiftwidth=3 tabstop=3 expandtab tw=72:
-// $Id$
-//
-// SimpleTest: real test of the algorithm.
-// Copyright (C) 2003  Casey Marshall <rsdio@metastatic.org>
-//
-// This file is a part of Jarsync
-//
-// Jarsync is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2 of the License, or (at
-// your option) any later version.
-//
-// Jarsync is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Jarsync; if not, write to the
-//
-//    Free Software Foundation, Inc.,
-//    59 Temple Place, Suite 330,
-//    Boston, MA  02111-1307
-//    USA
-//
-// Linking this library statically or dynamically with other modules is
-// making a combined work based on this library.  Thus, the terms and
-// conditions of the GNU General Public License cover the whole
-// combination.
-//
-// As a special exception, the copyright holders of this library give
-// you permission to link this library with independent modules to
-// produce an executable, regardless of the license terms of these
-// independent modules, and to copy and distribute the resulting
-// executable under terms of your choice, provided that you also meet,
-// for each linked independent module, the terms and conditions of the
-// license of that module.  An independent module is a module which is
-// not derived from or based on this library.  If you modify this
-// library, you may extend this exception to your version of the
-// library, but you are not obligated to do so.  If you do not wish to
-// do so, delete this exception statement from your version.
-//
-// --------------------------------------------------------------------------
+/* vim:set softtabstop=3 shiftwidth=3 tabstop=3 expandtab tw=72:
+   $Id$
+  
+   SimpleTest: real test of the algorithm.
+   Copyright (C) 2003  Casey Marshall <rsdio@metastatic.org>
+  
+   This file is a part of Jarsync
+  
+   Jarsync is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2 of the License, or (at
+   your option) any later version.
+  
+   Jarsync is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with Jarsync; if not, write to the
+  
+      Free Software Foundation, Inc.,
+      59 Temple Place, Suite 330,
+      Boston, MA  02111-1307
+      USA
+  
+   Linking Jarsync statically or dynamically with other modules is
+   making a combined work based on Jarsync.  Thus, the terms and
+   conditions of the GNU General Public License cover the whole
+   combination.
+  
+   As a special exception, the copyright holders of Jarsync give you
+   permission to link Jarsync with independent modules to produce an
+   executable, regardless of the license terms of these independent
+   modules, and to copy and distribute the resulting executable under
+   terms of your choice, provided that you also meet, for each linked
+   independent module, the terms and conditions of the license of that
+   module.  An independent module is a module which is not derived from
+   or based on Jarsync.  If you modify Jarsync, you may extend this
+   exception to your version of it, but you are not obligated to do so.
+   If you do not wish to do so, delete this exception statement from
+   your version.  */
 
 // Tags: JARSYNC
 
@@ -60,12 +58,14 @@ import org.metastatic.rsync.*;
  *
  * @version $Revision $
  */
-public class SimpleTest implements Testlet {
+public class SimpleTest implements Testlet, MatcherListener {
 
    // Fields.
    // -----------------------------------------------------------------------
 
    Random rand;
+
+   List deltas;
 
    // Constructor.
    // -----------------------------------------------------------------------
@@ -76,6 +76,10 @@ public class SimpleTest implements Testlet {
 
    // Instance methods.
    // -----------------------------------------------------------------------
+
+   public void update(Delta d) {
+      deltas.add(d);
+   }
 
    public void test(TestHarness harness) {
       String[] mds = getMessageDigests();
@@ -89,24 +93,65 @@ public class SimpleTest implements Testlet {
       } catch (Exception x) {
          throw new Error(x);
       }
-      conf.weakSum = new Checksum32();
       conf.strongSumLength = conf.strongSum.getDigestLength();
+
+      assert conf.strongSumLength != 0;
 
       for (int i = 0; i < 50; i++) {
          conf.blockLength = rand.nextInt(1400) + 250;
+         conf.weakSum = new Checksum32();
          byte[] n3w = new byte[rand.nextInt(1000000) + 500];
          rand.nextBytes(n3w);
-         byte[] old = mutate(n3w, harness);
+         byte[] old = null;
+         try {
+            old = mutate(n3w, harness);
+         } catch (Exception x) {
+            throw new Error(x);
+         }
          harness.verbose("TEST #" + (i+1) + ": old data=" + old.length
             + " bytes, target data=" + n3w.length + " bytes, blocks="
             + conf.blockLength + " bytes, digest="
             + conf.strongSum.getAlgorithm());
          Generator gen = new Generator(conf);
-         Collection sums = gen.generateSums(old);
-         Matcher mat = new Matcher(conf);
-         Collection deltas = mat.hashSearch(sums, n3w, 0);
+         List sums = gen.generateSums(old);
+         harness.verbose("\tGenerated " + sums.size() + " checksums.");
+         deltas = new LinkedList();
+         MatcherStream mat = new MatcherStream(conf);
+         mat.addListener(this);
+         mat.setChecksums(sums);
+         mat.update(n3w);
+         mat.doFinal();
+         int copies = 0, inserts = 0;
+         for (Iterator it = deltas.iterator(); it.hasNext(); ) {
+            if (it.next() instanceof DataBlock)
+               inserts++;
+            else
+               copies++;
+         }
+         harness.verbose("\tDeltas: " + copies + " copy commands, "
+            + inserts + " insert commands.");
          byte[] reconst = Rebuilder.rebuild(old, deltas);
          harness.check(Arrays.equals(n3w, reconst));
+         if (!Arrays.equals(n3w, reconst)) {
+            // Try to see why.
+            if (n3w.length != reconst.length) {
+               harness.debug("\tThe arrays are not of equal length!");
+               harness.debug("\t    n3w=" + n3w.length);
+               harness.debug("\treconst=" + reconst.length);
+               harness.debug("\tThe two differ in size by " +
+                  (n3w.length - reconst.length) + " bytes.");
+            } else {
+               for (int x = 0; x < n3w.length; x++) {
+                  if (n3w[x] != reconst[x]) {
+                     harness.debug("diff: index=" + x +
+                        " n3w=" + Util.toHexString(n3w, x, 1) +
+                        " reconst=" + Util.toHexString(reconst, x, 1)
+                     //   + " " + y + " contiguous mismatched blocks so far."
+                        + " blockLength=" + conf.blockLength);
+                  }
+               }
+            }
+         }
          try {
             conf.strongSum = MessageDigest.getInstance(
                mds[rand.nextInt(mds.length)]);
@@ -124,8 +169,9 @@ public class SimpleTest implements Testlet {
     * Derived from `mutate.pl' from librsync, (C) 1999, 2000 by Martin
     * Pool and (C) 1999 by Andrew Tridgell.
     */
-   private byte[] mutate(byte[] b, TestHarness harness) {
-      StringBuffer corpus = new StringBuffer(new String(b));
+   private byte[] mutate(byte[] b, TestHarness harness)
+   throws Exception {
+      StringBuffer corpus = new StringBuffer(new String(b, "ISO-8859-1"));
       int nmuts = 1 + rand.nextInt(30);
       harness.verbose("\t" + nmuts + " mutations");
       while (--nmuts > 0) {
@@ -154,7 +200,7 @@ public class SimpleTest implements Testlet {
                break;
          }
       }
-      return corpus.toString().getBytes();
+      return corpus.toString().getBytes("ISO-8859-1");
    }
 
    private String[] getMessageDigests() {
@@ -169,6 +215,6 @@ public class SimpleTest implements Testlet {
             algs.add(tries[i]);
          } catch (Exception x) { }
       }
-      return (String[]) algs.toArray(tries);
+      return (String[]) algs.toArray(new String[algs.size()]);
    }
 }
