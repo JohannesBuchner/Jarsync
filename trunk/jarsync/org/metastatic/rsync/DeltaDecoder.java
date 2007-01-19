61e7acd19544dcb544ce6de53937e7e153bf7768
rsdio <rsdio@388fd5d1-5327-0410-80e4-b6d47a689578>
2007-01-19 18:31:46 +0000

    Modified the license of jarsync to be the GPL+exception/ASL-2.0 disjunction.
    git-svn-id: https://jarsync.svn.sourceforge.net/svnroot/jarsync@185 388fd5d1-5327-0410-80e4-b6d47a689578

--- trunk/jarsync/org/metastatic/rsync/DeltaDecoder.java
+++ trunk/jarsync/org/metastatic/rsync/DeltaDecoder.java
@@ -1,7 +1,5 @@
 /* DeltaDecoder -- decodes Delta objects from external representations.
-   $Id$
-
-Copyright (C) 2003  Casey Marshall <rsdio@metastatic.org>
+   Copyright (C) 2003, 2007  Casey Marshall <rsdio@metastatic.org>
 
 This file is a part of Jarsync.
 
@@ -16,12 +14,8 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
 You should have received a copy of the GNU General Public License
-along with Jarsync; if not, write to the
-
-   Free Software Foundation, Inc.,
-   59 Temple Place, Suite 330,
-   Boston, MA  02111-1307
-   USA
+along with Jarsync; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 Linking Jarsync statically or dynamically with other modules is making
 a combined work based on Jarsync.  Thus, the terms and conditions of
@@ -37,7 +31,24 @@ module.  An independent module is a module which is not derived from
 or based on Jarsync.  If you modify Jarsync, you may extend this
 exception to your version of it, but you are not obligated to do so.
 If you do not wish to do so, delete this exception statement from your
-version.  */
+version.
+
+ALTERNATIVELY, Jarsync may be licensed under the Apache License,
+Version 2.0 (the "License"); you may not use this file except in
+compliance with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.  See the License for the specific language governing
+permissions and limitations under the License.
+
+If you modify Jarsync, you may extend this same choice of license for
+your library, but you are not obligated to do so. If you do not offer
+the same license terms, delete the license terms that your library is
+NOT licensed under.  */
 
 
 package org.metastatic.rsync;
